c331271f1b188ddd45940794c86f3e25b556c475
Olya <koraeva.olya@yandex.ru>
2021-09-03 19:14:59 +0300

    Исправлено лишнее puts

--- number_les8.rb
+++ number_les8.rb
@@ -2,7 +2,7 @@ def number
 	num = ARGV
 	if num.map { |i| i.to_i.to_s } == num
 		num.map! { |i| i.to_i}
-		puts 	"Все числа целые, самое большое: #{num.max}" if num.all? { |i| i.integer? }
+		"Все числа целые, самое большое: #{num.max}" if num.all? { |i| i.integer? }
 	else 
 		'Что-то не подходит'
 	end
